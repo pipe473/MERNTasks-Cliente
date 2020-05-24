15529ddd0b5dc4290eb2488fc0d59d795bcf90e6
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-24 12:48:35 +0200

    Creando estructura del sidebar para el componente de proyectos

--- src/components/proyectos/Proyectos.js
+++ src/components/proyectos/Proyectos.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Sidebar from '../layout/Sidebar';
 
 const Proyectos = () => {
     return ( 
         <div className="contenedor-app">
-            
+                <Sidebar />
             <div className="seccion-principal">
                 <main>
                     <div className="contenedor-tareas">
