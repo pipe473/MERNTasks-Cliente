b73d1130f128a4602cac3fdcbdae9d0f352d5c87
pipe473 <bedoyafelipe743@gmail.com>
2020-06-09 18:07:07 +0200

    Resolviendo uno de los problemas con las keys. y

--- src/components/proyectos/ListadoProyectos.js
+++ src/components/proyectos/ListadoProyectos.js
@@ -4,13 +4,18 @@ import proyectoContext from "../../context/proyectos/proyectoContext";
 
 const ListadoProyectos = () => {
 
+    // Extraer proyectos de state inicial
     const proyectosContext = useContext(proyectoContext);
     const { proyectos } = proyectosContext;
+
+    // Revisar si proyectos tiene contenido
+    if(proyectos.length === 0 ) return null;
 
     return ( 
         <ul className="listado-proyectos">
             {proyectos.map(proyecto => (
                 <Proyecto 
+                    key={proyecto.id}
                     proyecto={proyecto}
                 />
             ))}
