5c90422029fe6618e2228f16adaa4a2b6ca1dcd1
pipe473 <bedoyafelipe743@gmail.com>
2020-06-09 15:52:18 +0200

    Trabajando con la interfaz de tareas

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -26,6 +26,11 @@ const ListadoTareas = () => {
                 
             }
             </ul>
+
+            <button
+                type="button"
+                className="btn btn-eliminar"
+            >Eliminar Proyecto &times;</button>
         </Fragment>
      );
 }
