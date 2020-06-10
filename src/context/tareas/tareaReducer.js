e47ff57f10fbdae496955c7e3dd997ae2352c4a7
pipe473 <bedoyafelipe743@gmail.com>
2020-06-10 18:47:22 +0200

    Creando un dispatch para nuevas tareas

--- src/context/tareas/tareaReducer.js
+++ src/context/tareas/tareaReducer.js
@@ -1,6 +1,7 @@
 
 import {
-    TAREAS_PROYECTO 
+    TAREAS_PROYECTO,
+    AGREGAR_TAREA 
 } from '../../types';
 
 export default (state, action) => {
@@ -10,6 +11,11 @@ export default (state, action) => {
                 ...state,
                 tareasproyecto: state.tareas.filter(tarea => tarea.proyectoId ===
                 action.payload)
+            }
+        case AGREGAR_TAREA:
+            return {
+                ...state,
+                tareas: [...state.tareas, action.payload]
             }
 
 
