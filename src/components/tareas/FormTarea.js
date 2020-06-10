4ffac63bc3a4c450f2edca3d909fa568a23d439e
pipe473 <bedoyafelipe743@gmail.com>
2020-06-10 18:59:06 +0200

    Insertando tareas en el proyecto actual

--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -1,5 +1,6 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import proyectoContext from "../../context/proyectos/proyectoContext";
+import tareaContext from "../../context/tareas/tareaContext";
 
 const FormTarea = () => {
 
@@ -7,11 +8,32 @@ const FormTarea = () => {
     const proyectosContext = useContext(proyectoContext);
     const { proyecto } = proyectosContext;
 
+     // Obtener la función del context de tarea
+     const tareasContext = useContext(tareaContext);
+     const { agregarTarea } = tareasContext;
+
+    // State del formulario
+    const [tarea, guardarTarea] = useState({
+        nombre: ''
+    })
+
+    // Extraer el nombre del proyecto
+    const { nombre } = tarea;
+
     // Si no hay proyecto seleccionado
     if(!proyecto) return null;
 
     // Array destructuring para extraer el proyecto actual
     const [proyectoActual] = proyecto;
+
+
+    // Leer los valores del formulario
+    const handleChange = e => {
+        guardarTarea({
+            ...tarea,
+            [e.target.name] : e.target.value
+        })
+    }
 
 
     const onSubmit = e => {
@@ -22,6 +44,9 @@ const FormTarea = () => {
         // Pasar la validación
 
         // Agregar la nueva tarea al state de tareas
+        tarea.proyectoId = proyectoActual.id;
+        tarea.estado = false;
+        agregarTarea(tarea);
 
         // Reiniciar el form
     }
@@ -37,6 +62,8 @@ const FormTarea = () => {
                         className="input-text"
                         placeholder="Nombre Tarea..."
                         name="nombre"
+                        value={nombre}
+                        onChange={handleChange}
                     />
                 </div>
                 <div className="contenedor-input">
