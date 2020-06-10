54c037015a8faf33a78593acbe59864e63e7fee6
pipe473 <bedoyafelipe743@gmail.com>
2020-06-10 18:28:06 +0200

    Creando una funcion que obtenga las tareas del proyecto

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -1,49 +1,48 @@
-import React, { Fragment, useContext } from 'react';
-import Tarea from './Tarea';
+import React, { Fragment, useContext } from "react";
+import Tarea from "./Tarea";
 import proyectoContext from "../../context/proyectos/proyectoContext";
 
 const ListadoTareas = () => {
+  // Extraer proyectos de state inicial
+  const proyectosContext = useContext(proyectoContext);
+  const { proyecto, eliminarProyecto } = proyectosContext;
 
-    // Extraer proyectos de state inicial
-    const proyectosContext = useContext(proyectoContext);
-    const { proyecto, eliminarProyecto } = proyectosContext;
+  // Si no hay proyecto seleccionado
+  if (!proyecto) return <h2>Seleccion un proyecto</h2>;
 
-    // Si no hay proyecto seleccionado
-    if(!proyecto) return <h2>Seleccion un proyecto</h2>;
+  // Array destructuring para extraer el proyecto actual
+  const [proyectoActual] = proyecto;
 
-    // Array destructuring para extraer el proyecto actual
-    const [proyectoActual] = proyecto;
+  const tareasProyecto = [];
 
-    const tareasProyecto = [];
+  // Elimina un proyecto
+  const onClickEliminar = () => {
+    eliminarProyecto(proyectoActual.id);
+  };
 
-    // Elimina un proyecto
-    const onClickEliminar = () => {
-        eliminarProyecto(proyectoActual.id)
-    }
+  return (
+    <Fragment>
+      <h2>Proyecto: {proyectoActual.nombre}</h2>
 
-    return ( 
-        <Fragment>
-            <h2>Proyecto: {proyectoActual.nombre}</h2>
+      <ul className="listado-tareas">
+        {tareasProyecto.length === 0 ? (
+          <li className="tarea">
+            <p>No hay tareas</p>
+          </li>
+        ) : (
+          tareasProyecto.map((tarea) => <Tarea tarea={tarea} />)
+        )}
+      </ul>
 
-            <ul className="listado-tareas">
-                {tareasProyecto.length === 0 
-                ? (<li className="tarea"><p>No hay tareas</p></li>)
-                : tareasProyecto.map(tarea => (
-                    <Tarea 
-                        tarea={tarea}
-                    />
-                ))
-                
-            }
-            </ul>
+      <button
+        type="button"
+        className="btn btn-eliminar"
+        onClick={onClickEliminar}
+      >
+        Eliminar Proyecto &times;
+      </button>
+    </Fragment>
+  );
+};
 
-            <button
-                type="button"
-                className="btn btn-eliminar"
-                onClick={ onClickEliminar }
-            >Eliminar Proyecto &times;</button>
-        </Fragment>
-     );
-}
- 
 export default ListadoTareas;
--- src/context/tareas/tareaState.js
+++ src/context/tareas/tareaState.js
@@ -1,42 +1,53 @@
+import React, { useReducer } from "react";
+import TareaContext from "./tareaContext";
+import TareaReducer from "./tareaReducer";
 
-import React, { useReducer } from 'react';
-import TareaContext from './tareaContext';
-import TareaReducer from './tareaReducer';
+import {
+     TAREAS_PROYECTO 
+} from '../../types';
 
-const TareaState = props => {
-    const initialState = {
-        tareas: [
-            {nombre: 'Elegir plataforma', estado: true, proyectoId: 1 },
-            {nombre: 'Elegir Colores', estado: false, proyectoId: 2 },
-            {nombre: 'Elegir Plataformas de pago', estado: false, proyectoId: 3 },
-            {nombre: 'Elegir Hosting', estado: true, proyectoId: 4 },
-            {nombre: 'Elegir plataforma', estado: true, proyectoId: 1 },
-            {nombre: 'Elegir Colores', estado: false, proyectoId: 2 },
-            {nombre: 'Elegir Plataformas de pago', estado: false, proyectoId: 3 },
-            {nombre: 'Elegir plataforma', estado: true, proyectoId: 4 },
-            {nombre: 'Elegir Colores', estado: false, proyectoId: 1 },
-            {nombre: 'Elegir Plataformas de pago', estado: false, proyectoId: 2 },
-            {nombre: 'Elegir plataforma', estado: true, proyectoId: 3 },
-            {nombre: 'Elegir Colores', estado: false, proyectoId: 4 },
-            {nombre: 'Elegir Plataformas de pago', estado: false, proyectoId: 3 },
-        ],
-    }
+const TareaState = (props) => {
+  const initialState = {
+    tareas: [
+      { nombre: "Elegir plataforma", estado: true, proyectoId: 1 },
+      { nombre: "Elegir Colores", estado: false, proyectoId: 2 },
+      { nombre: "Elegir Plataformas de pago", estado: false, proyectoId: 3 },
+      { nombre: "Elegir Hosting", estado: true, proyectoId: 4 },
+      { nombre: "Elegir plataforma", estado: true, proyectoId: 1 },
+      { nombre: "Elegir Colores", estado: false, proyectoId: 2 },
+      { nombre: "Elegir Plataformas de pago", estado: false, proyectoId: 3 },
+      { nombre: "Elegir plataforma", estado: true, proyectoId: 4 },
+      { nombre: "Elegir Colores", estado: false, proyectoId: 1 },
+      { nombre: "Elegir Plataformas de pago", estado: false, proyectoId: 2 },
+      { nombre: "Elegir plataforma", estado: true, proyectoId: 3 },
+      { nombre: "Elegir Colores", estado: false, proyectoId: 4 },
+      { nombre: "Elegir Plataformas de pago", estado: false, proyectoId: 3 },
+    ],
+  };
 
-    // Crear dispatch y state
-    const [state, dispatch] = useReducer(TareaReducer, initialState);
+  // Crear dispatch y state
+  const [state, dispatch] = useReducer(TareaReducer, initialState);
 
-    return (
-        <TareaContext.Provider
-            value={{
-                tareas: state.tareas
-            }}
-        >
-            {props.children}
-        </TareaContext.Provider>
-    )
-}
+  // Crear las funciones
+
+  // Obtener las tareas de un proyecto
+  const obtenerTareas = proyectoId => {
+      dispatch({
+          type: TAREAS_PROYECTO,
+          payload: proyectoId
+      })
+  }
+
+  return (
+    <TareaContext.Provider
+      value={{
+        tareas: state.tareas,
+        obtenerTareas
+      }}
+    >
+      {props.children}
+    </TareaContext.Provider>
+  );
+};
 
 export default TareaState;
-
-
-
