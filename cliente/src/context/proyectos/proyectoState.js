5d1207b0381b4596b14d46bd7474c9f4df7da264
pipe473 <bedoyafelipe743@gmail.com>
2020-06-19 18:13:02 +0200

    Eliminando los proyectos

--- cliente/src/context/proyectos/proyectoState.js
+++ cliente/src/context/proyectos/proyectoState.js
@@ -84,11 +84,17 @@ const ProyectoState = props => {
     }
 
     // Elimina un proyecto
-    const eliminarProyecto = proyectoId => {
-        dispatch({
-            type:  ELIMINAR_PROYECTO,
-            payload: proyectoId
-        })
+    const eliminarProyecto = async proyectoId => {
+        try {
+            await clienteAxios.delete(`/api/proyectos/${proyectoId}`); 
+            dispatch({
+                type:  ELIMINAR_PROYECTO,
+                payload: proyectoId
+            });
+        } catch (error) {
+            console.log(error);
+            
+        }
     }
 
 
